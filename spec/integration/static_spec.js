b7eef2aa4dc6b0be795e89af409b01302b763814
Mai Huynh <nhumaihuynh91@gmail.com>
2018-11-12 23:22:01 -0600

    express-assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,31 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const base = "http://localhost:3000";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
 
-//#2
     it("should return status code 200", (done) => {
 
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
-//#4
         done();
       });
     });
 
   });
+
+  describe("/marco", () => {
+    it("should return status code 200", (done) => {
+
+      request.get(`${base}/marco`, (err, res, body) =>{
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe('polo');
+        done();
+      });
+    });
+    
+  })
 });

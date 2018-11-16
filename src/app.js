6b5acf3f29432d87f0671782d9c83980bd8fd9eb
Mai Huynh <nhumaihuynh91@gmail.com>
2018-11-16 12:22:46 -0600

    topic resource2

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ const app = express();
 
 const routeConfig = require("./config/route-config.js");
 const appConfig = require("./config/main-config.js");
-
-routeConfig.init(app);
 appConfig.init(app, express);
-
+routeConfig.init(app);
 module.exports = app;

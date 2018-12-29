2f7b1eab230446917e9401de9328e465c789a4b5
Mai Huynh <nhumaihuynh91@gmail.com>
2018-12-28 21:58:20 -0600

    Finish TDD for Search Book

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,34 +5,27 @@ module.exports = {
   index(req, res, next) {
     let page = req.query._page?+req.query._page:null; //+ to become a number
     let limit = req.query._limit?+req.query._limit:null;
-
-    bookQueries.countAll((err, total) => {
+    let title_like = req.query.title_like?req.query.title_like:null;
+    //filters: filters sent from client
+    let filters = {page,limit,title_like};
+    bookQueries.countBooks(filters.title_like,(err, total) => {
       if (err) {
         console.log(err);
         res.json(500, { "error": "Internal Server Error" });
       } else {
+        //set header: set total to key totalHeader which is x-total-count
         res.set(totalHeader, total);  //res.setHeader
-        if (page == null || limit == null) {
-          bookQueries.getAllBooks((err, books) => {
+        if(filters.page === 0){
+          res.json([]);
+        }
+        else{
+          bookQueries.getBooks(filters,(err, books) => {
             if (err) {
               console.log(err);
               res.json(500, { "error": "Internal Server Error" });
             } else {
-              res.json(books);
-            }
-          })
-        } else {
-          switch (page) {
-            case 0: res.json([]); break;
-            default: bookQueries.getBooks(page, limit, (err, books) => {
-              if (err) {
-                console.log(err);
-                res.json(500, { "error": "Internal Server Error" });
-              } else {
-                res.json(books)
-              }
-            }); break;
-          }
+              res.json(books)
+            }})
         }
       }
     });
--- src/db/queries.books.js
+++ src/db/queries.books.js
@@ -1,28 +1,42 @@
 const fs = require("fs");
 const Book = require("./models").Book;
-
+const Sequelize = require("../../src/db/models/index").Sequelize;
 module.exports = {
   //loaded from db
-  getAllBooks(callback) {
-    return Book.all()
-      .then((books) => {
-        callback(null, books);
-      })
+  getBooks(filters, callback) {
+    //filters: from react/client, 
+    //queryFilter: transform client filters to filters that can be unserstood by sequelize
+    let queryFilter = {};
+    //if title_like:select * from books where title like '%title_like%';
+    if (filters.title_like) {
+      queryFilter.where = { title: { [Sequelize.Op.iLike]: `%${filters.title_like}%` } }
+    }
+    //if no title_like:select * from books';
+    //if page and limit: select * from books offset (filters.page-1) limit (filters.limit);
+    if (filters.page && filters.limit) {
+      queryFilter.offset = filters.page - 1;
+      queryFilter.limit = filters.limit;
+      queryFilter.order = [['id', 'ASC']];
+    }
+    //if no page or limit:select * from books;
+    Book.findAll(queryFilter).then((books) => {
+      callback(null, books);
+    })
       .catch((err) => {
         callback(err);
       })
   },
 
-  getBooks(page, limit, callback) {
-    return Book.findAll({ offset: page-1, limit: limit,order:[['id', 'ASC']]}) //for pagination
-      .then(books => callback(null, books))
-      .catch(err => callback(err))
-  },
-
-  countAll(callback) {
-    return Book.count()
+  countBooks(title_like, callback) { //count total item that sent from server (x-total-count)
+    //queryFilter: for sequelize
+    let queryFilter = {};
+    //like sql: select * from books where title like %title_like%
+    if (title_like) {
+      queryFilter.where = { title: { [Sequelize.Op.iLike]: `%${title_like}%` } }
+    }
+    //if no title_like: select * from books
+    return Book.count(queryFilter)
       .then(total => callback(null, total))
       .catch(err => callback(err))
-  },
-
+  }
 }
